635fb85385fcbf67882cf7ba8a240e3907e097c0
ComminsT <81898852+ComminsT@users.noreply.github.com>
2022-02-02 10:22:07 +0100

    export_afpa

---
+++ Reporter3/src/main/java/direction/Header.java
@@ -0,0 +1,91 @@
+package direction;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.bind.DatatypeConverter;
+
+import models.Database;
+import models.Utilisateur;
+
+/**
+ * Servlet implementation class Header
+ */
+@WebServlet(name = "HeaderD", urlPatterns = { "/Direction/Header" })
+public class Header extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Header() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Boolean change=false;
+		Boolean problem=false;
+		Database.Connect();
+		
+		HttpSession session = request.getSession(true);
+		Utilisateur utilisateur = (Utilisateur)session.getAttribute("currentuser");
+		System.out.println(session.getAttribute("isConnected"));
+		if (session.getAttribute("isConnected") == null) {
+			session.setAttribute("isConnected", false);
+		}
+		if(request.getParameter("changemdp")!=null) {
+			System.out.println("Entrée dans changemdp");
+			String cmdp1 = request.getParameter("mdp");
+			System.out.println("cmdp="+cmdp1);
+			String cmdp=cmdp1+"zack"+(utilisateur.getIdentifiant().length()*5-3);
+			String myHash="";
+			MessageDigest md;
+			try {
+				md = MessageDigest.getInstance("MD5");
+				md.update(cmdp.getBytes());
+				byte[] digest = md.digest();
+				myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			System.out.println("MyHash = "+myHash);
+			System.out.println("pwd ="+utilisateur.getPassword());
+			if(myHash.equals(utilisateur.getPassword())) {
+				System.out.println("EQUALS");
+				String newmdp=request.getParameter("newmdp");
+				utilisateur.changemdp(newmdp);
+			}else {
+				System.out.println("NOT EQUALS");
+				problem=true;
+					
+			}
+			
+			
+			
+			
+		}
+			request.getRequestDispatcher("/Direction/Header.jsp").include(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Reporter3/src/main/java/plateforme/Reporting2.java
@@ -0,0 +1,50 @@
+package plateforme;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import verif.Checker;
+
+/**
+ * Servlet implementation class Reporting2
+ */
+@WebServlet(name = "Reporting2", urlPatterns = { "/Plateforme/Reporting2" })
+public class Reporting2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Reporting2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		if(request.getSession(false) == null) {
+		    response.sendRedirect("/Reporter2/Login");
+		}else if (!Checker.usercheck(request, 3)) {
+			response.sendRedirect("/Reporter2/Login");
+		} else {
+		System.out.println("Avant récupération de l'id_action");
+		int id_action =Integer.parseInt(request.getParameter("id"));
+		request.getRequestDispatcher( "/Plateforme/reporting2.jsp" ).forward( request, response );
+	}}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
